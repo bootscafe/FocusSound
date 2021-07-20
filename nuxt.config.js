9a8aaaec61240ff202f039ee536cf5f5c1a8b67d
Kyosuke Kondo <kyosuke@bootscafe.jp>
2021-07-20 10:00:19 +0900

    初期セットアップ

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,12 @@
+const Sass = require('sass')
+const Fiber = require('fibers')
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'FocusSound',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
@@ -12,12 +15,14 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap' },
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/scss/base.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -35,12 +40,29 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    'nuxt-microcms-module'
   ],
+
+  microcms: {
+    options: {
+      serviceDomain: "focussound", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
+      apiKey: "0fca7455-f6ba-4050-b47f-3e53959236b3",
+    },
+    mode: process.env.NODE_ENV === 'production' ? 'server' : 'all',
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    loaders: {
+      scss: {
+        implementation: Sass,
+        sassOptions: {
+          fiber: Fiber
+        }
+      }
+    }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <Tutorial/>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $microcms }) {
+    const data = await $microcms.get({
+      endpoint: 'select',
+      queries: { limit: 20, filters: 'createdAt[greater_than]2021' },
+    });
+    return data;
+  }
+}
 </script>
+
+<style lang='scss' scoped>
+  ul {
+    li {
+      font-size: 1.5rem;
+    }
+  }
+</style>
