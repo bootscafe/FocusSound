ae299c442b531dd66899afb0881e50127317f339
Kyosuke Kondo <kyosuke@bootscafe.jp>
2021-07-28 10:34:09 +0900

    レイアウトコンポーネントを設定

---
+++ components/layouts/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header id="header">
+    <nav>
+      <nuxt-link to="/select/">Select</nuxt-link>
+      <nuxt-link to="/review/">Review</nuxt-link>
+      <nuxt-link to="/news/">News</nuxt-link>
+      <nuxt-link to="/feature/">Feature</nuxt-link>
+    </nav>
+  </header>
+</template>
+
+<style lang='scss' scoped>
+#header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 60px;
+  background-color: #EBEBEB;
+}
+
+nav {
+  font-family: times;
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  a {
+    margin: 0 1em;
+  }
+}
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="contaienr">
+      <Header/>
+      <Nuxt id="nuxt" />
+      <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from '../components/layouts/Header'
+import Footer from '../components/layouts/Footer'
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
